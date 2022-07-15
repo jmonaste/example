664e77a645bd6a260d9bdaa65a716c6c0dd500e4
javier_raspberrypi <javiermonasterio@gmail.com>
2022-07-15 08:53:51 +0200

    Otro ejemplo de ejecución en python con condicionales

---
+++ example3.py
@@ -0,0 +1,7 @@
+edad = int(input("Escribe tu edad: "))
+if edad > 17:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+#siempre que ponemos dos puntos, debajo hay que poner cuatro espacios
